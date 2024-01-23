919920cfcc636836799f52fb7327e21489879696
IN1kky <Sigitov.349@yandex.ru>
2024-01-23 11:27:12 +0300

    Добавлен многомерный массив (4)

---
+++ Task3/work.js
@@ -0,0 +1,6 @@
+const auto = [["Pagani","Chevrolet","Dodge","Ford","Toyota","Nissan"],["YAMAHA","Honda","Ducati","MTT"],["T90","T-34-85","Abrams","BMP-2","Tiger"],["AMD","Ryzen","Intel","Acer"]];
+console.log(auto);
+console.log(auto[0]);
+console.log(auto[1][3]);
+console.log(auto[2]);
+console.log(auto[3][0]);
