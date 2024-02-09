859e40ad8f3b11385565fefe5c2ec0d6ba2a8140
IN1kky <Sigitov.349@yandex.ru>
2024-02-09 09:51:16 +0300

    Start HTML

---
+++ Task10/app.js
@@ -0,0 +1,4 @@
+const text = document.querySelector(".block .text");
+
+console.log(text);                                      // Весь блок с указанным классом
+console.log(text.innerText);                            // Только текст
